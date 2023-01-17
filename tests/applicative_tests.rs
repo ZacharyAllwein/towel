220c3bc79227ddbc7570db71e1abc34101c45153
Zachary Allwein <zrallwein@gmail.com>
2023-01-17 09:59:25 -0600

    test

--- tests/applicative_tests.rs
+++ tests/applicative_tests.rs
@@ -11,10 +11,7 @@ fn mul_two(x: &i32) -> i32 {
 fn vec_applicative() {
     let f_vec: Vec<&dyn Fn(&i32) -> i32> = vec![&|x| x + 1, &|x| x * 2];
 
-    assert_eq!(
-        vec![1, 2].app(&f_vec),
-        vec![2, 3, 2, 4]
-    )
+    assert_eq!(vec![1, 2].app(&f_vec), vec![2, 3, 2, 4])
 }
 
 #[test]
