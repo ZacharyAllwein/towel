faf288cbb4ecaa6c5b46d9d6ad666bb186cef206
axenna <zrallwein@gmail.com>
2023-01-19 06:59:29 -0600

    commit all changes(I think I didn't include tests last time

--- src/traits/monad.rs
+++ src/traits/monad.rs
@@ -2,7 +2,6 @@ use crate::traits::applicative::Applicative;
 use crate::traits::functor::Functor;
 
 pub trait Monad<'a, A>: Applicative<'a, A> {
-
     fn ret(a: A) -> Self
     where
         Self: Sized,
@@ -15,7 +14,6 @@ pub trait Monad<'a, A>: Applicative<'a, A> {
 
 //basically a flat map for vec
 impl<'a, A: 'a> Monad<'a, A> for Vec<A> {
-
     fn bind<B, F: Fn(&A) -> Self::HKT<B>>(&self, f: F) -> Self::HKT<B> {
         self.fmap(f).into_iter().flatten().collect()
     }
@@ -23,7 +21,6 @@ impl<'a, A: 'a> Monad<'a, A> for Vec<A> {
 
 //map value inside Option to Option then reduce structure
 impl<'a, A: 'a> Monad<'a, A> for Option<A> {
-
     fn bind<B, F: Fn(&A) -> Self::HKT<B>>(&self, f: F) -> Self::HKT<B> {
         match self {
             None => None,
--- tests/state_tests.rs
+++ tests/state_tests.rs
@@ -11,11 +11,25 @@ fn state_functor() {
 }
 
 #[test]
-fn state_applicative(){
+fn state_applicative() {
     let s1 = <State<'static, Vec<i32>, i32> as Applicative<i32>>::pure(1);
     let s2: State<'static, Vec<i32>, fn(&i32) -> i32> = State::new(Box::new(|s| (|&x| x + 1, s)));
     let s3 = s1.app(&s2);
 
+    assert_eq!(s3.eval(vec![1]), (2, vec![1]));
+}
 
-    assert_eq!(s3.eval(vec![1]), (2, vec![]));
+#[test]
+fn state_monad() {
+    let s1 = <State<'static, Vec<i32>, i32> as Monad<i32>>::ret(1);
+
+    let s2 = s1.bind(|&x| {
+        State::new(Box::new(move |mut s| {
+            let a = x + 1;
+            s.push(a);
+            (a, s)
+        }))
+    });
+
+    assert_eq!(s2.eval(vec![1]), (2, vec![1, 2]))
 }
