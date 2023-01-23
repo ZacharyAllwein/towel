5ef3ff7f9dcb8ed5e695e782b5a9b3c910f00c59
axenna <zrallwein@gmail.com>
2023-01-22 22:35:51 -0600

    Need to work on optionT, but after that I should have a solid model that can be implemented for anything

--- tests/monad_tests.rs
+++ tests/monad_tests.rs
@@ -1,20 +1,24 @@
 use towel::traits::Monad;
 
 #[test]
-fn vec_monad(){
-
+fn vec_monad() {
     let a: Vec<i32> = vec![1, 2, 3];
     let f: fn(i32) -> Vec<String> = |a| vec![a.to_string(), a.to_string()];
-    
+
     assert_eq!(a.bind(f), vec!["1", "1", "2", "2", "3", "3"]);
 }
 
 #[test]
-fn opt_monad(){
-
+fn opt_monad() {
     let a = Some(1);
 
-    let f: fn(i32) -> Option<String> = |a| if a < 10 { Some((a + 1).to_string()) } else { None };
+    let f: fn(i32) -> Option<String> = |a| {
+        if a < 10 {
+            Some((a + 1).to_string())
+        } else {
+            None
+        }
+    };
 
     assert_eq!(a.bind(f), Some("2".to_string()))
 }
--- tests/test.rs
+++ tests/test.rs
@@ -1,8 +1,8 @@
-use towel::prelude::{Either, Right};
 use towel::data_structures::OptionT;
+use towel::prelude::{Either, Right};
 
 #[test]
-fn option_t_init(){
+fn option_t_init() {
     let x = OptionT::<Either<Option<i32>, Option<i32>>, i32>::new(3);
     let y = OptionT::<Either<Option<i32>, Option<i32>>, i32>::new_manual(Right(Some(3)));
 
