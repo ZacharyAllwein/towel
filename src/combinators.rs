b1f9147656f9711b9fb218ddbfa2316872106077
axenna <zrallwein@gmail.com>
2023-01-24 16:10:02 -0600

    finished combinators

--- src/combinators.rs
+++ src/combinators.rs
@@ -20,8 +20,8 @@ pub fn thrush<A, F: FnOnce(A) -> B, B>(a: A, f: F) -> B {
 }
 
 /// passes one arg to a fn that takes two args twice
-pub fn duplication<F: FnOnce(A, A) -> B, A: Clone, B>(f: F, a: A) -> B {
-    f(a.clone(), a)
+pub fn duplication<F: Fn(A, A) -> B, A: Clone, B>(f: F) -> impl Fn(A) -> B {
+    move |a| f(a.clone(), a)
 }
 
 /// flips arguments for a fn around
@@ -37,4 +37,61 @@ where
     G: Fn(A) -> B,
 {
     move |a| f(g(a))
+}
+
+/// like composition but less similar to the mathematical definition
+/// works like unix |, passes output to input of next fn
+pub fn pipe<F, G, A, B, C>(f: F, g: G) -> impl Fn(A) -> C
+where
+    F: Fn(A) -> B,
+    G: Fn(B) -> C,
+{
+    move |a| g(f(a))
+}
+
+
+/// takes a function with two args and allows the second arg to be
+/// substituted with a function application on the first arg
+pub fn substitution<F, G, A, B, C>(f: F, g: G) -> impl Fn(A) -> C
+where
+    A: Clone,
+    F: Fn(A, B) -> C,
+    G: Fn(A) -> B,
+{
+    move |a| f(a.clone(), g(a))
+}
+
+/// allows composition where
+/// you need to have some constant environment(A)
+/// in a function
+pub fn chain<F, G, A, B, C>(f: F, g: G) -> impl Fn(A) -> C
+where
+    A: Clone,
+    F: Fn(A) -> B,
+    G: Fn(B, A) -> C,
+{
+    move |a| g(f(a.clone()), a)
+}
+
+/// takes two functions that take same arg, but have different outputs
+/// and a function that combines their outputs,
+/// and returns a fn that takes one input and returns the combined output
+pub fn converge<F, G, H, A, B, C, D>(f: F, g: G, h: H) -> impl Fn(A) -> D
+where
+    A: Clone,
+    F: Fn(B, C) -> D,
+    G: Fn(A) -> B,
+    H: Fn(A) -> C,
+{
+    move |a| f(g(a.clone()), h(a))
+}
+
+/// a sister to converge. returns a fn that takes two args runs them through a
+/// unary fn, then merges their outputs with a provided binary fn
+pub fn psi<F, G, A, B, C>(f: F, g: G) -> impl Fn(A, A) -> C
+where
+    F: Fn(B, B) -> C,
+    G: Fn(A) -> B,
+{
+    move |a, b| f(g(a), g(b))
 }
